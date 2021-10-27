4bfd6247b7cbc13afa893ff089b8ec3f7d698c95
Aastha Savaliya <aasthasavaliya26@gmail.com>
2021-10-27 22:40:14 +0530

    second commit

---
+++ src/client.js
@@ -0,0 +1,6 @@
+import * as contentful from 'contentful'
+
+export const client = contentful.createClient({
+    space: 'i5po4o4kx8m5',
+    accessToken: 'zPLg2Qb8lESOXzLcn8TN6kC4VDNATvO8jcCxgvcf9dg'
+})
